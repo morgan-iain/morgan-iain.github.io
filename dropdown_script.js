4e2b5859b457bc39b62c7785b1c033a89a6bf060
iain <iainmorgan1208@gmail.com>
2025-06-07 22:25:45 -0700

    fix project dropdown button

--- dropdown_script.js
+++ dropdown_script.js
@@ -1,5 +1,6 @@
 // Toggle the dropdown when the button or its icon is clicked
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
+{
   const button = document.getElementById("dropdownButton");
   const dropdown = document.getElementById("myDropdown");
 
@@ -8,11 +9,17 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Close the dropdown if the user clicks outside of it
-  window.addEventListener("click", function (e) {
-    if (!button.contains(e.target)) {
-      if (dropdown.classList.contains("show")) {
+  window.addEventListener("click", function (e) 
+  {
+    const button = document.getElementById("dropdownButton");
+    const dropdown = document.getElementById("myDropdown");
+    if (button.contains(e.target)) 
+    {
+        dropdown.classList.toggle("show");
+    }
+    else if (!dropdown.contains(e.target)) 
+    {
         dropdown.classList.remove("show");
-      }
     }
   });
 });
